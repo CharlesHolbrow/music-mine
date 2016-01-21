3a8f1d7a2633dbd8107c18eb6a2fb14ebdeec224
Charles Holbrow <charlesholbrow@gmail.com>
2016-01-21 00:49:46 -0500

    getdata is abstracted

--- get-data.js
+++ get-data.js
@@ -1,47 +1,24 @@
 'use strict';
 
-var fs      = require('fs')
-  , path    = require('path')
-  , url     = require('url')
-  , request = require('request')
+var path = require('path')
+  , echo = require('./echo-nest-request.js').echo
 
-// Don't check our secrets in to Git
-var secretsFilename = path.join(__dirname, 'secrets.json')
-var secrets = JSON.parse(fs.readFileSync(secretsFilename))
-
-// Construct api request url, starting with the query parameters
-var query = {
-  api_key:  secrets.echo_nest_api_key
-  , sort:   'song_hotttnesss-desc'
-  , bucket: ['song_hotttnesss', 'tracks', 'id:7digital-US']
-  // , style:  'pop'
-  , results: 100
-  , start:   0
+var params = {
+  sort:       'song_hotttnesss-desc'
+  , bucket:   ['song_hotttnesss', 'tracks', 'id:7digital-US']
+  , results:  100
+  , start:    0
+  // , style:    'pop'
 }
 
-var url = url.format({
-  hostname:   'developer.echonest.com'
-  , protocol: 'https'
-  , pathname: 'api/v4/song/search'
-  , query:    query
-})
+var filename = path.join(__dirname, 'data/hot-songs.json')
 
-console.log('requesting:', url)
-
-request(url, (err, res, body)=>{
-  if (err) {
-    console.log('Error getting results', err)
-    return
+echo(
+  'api/v4/song/search'
+  , params
+  , filename
+  , (err, result)=>{
+    console.error('err', err)
+    console.log(result)
   }
-  try {
-    let data = JSON.parse(body)
-    handleSongsResponse(data)
-  } catch (localError) {
-    console.error('Error processing body:', localError, body)
-    throw localError
-  }
-}).pipe(fs.createWriteStream(path.join(__dirname, 'data/hot-songs.json')))
-
-var handleSongsResponse = function(resJson) {
-  //
-}
+)
